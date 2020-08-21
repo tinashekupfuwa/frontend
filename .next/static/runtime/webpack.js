7d51f0cc0219f57d90ea0c0b91fde6223ed6e440
tinashekupfuwa <tinashekupfuwa@gmail.com>
2020-08-21 23:58:38 +0200

    added themes and styling

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bb56d541390d8d351892";
+/******/ 	var hotCurrentHash = "4b466c3349e2efed75fb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
